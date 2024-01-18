ea874995d253a44968ae7a50385774b5566e9790
Kotaro SONODA <kotaro1976@gmail.com>
2024-01-18 22:03:11 +0900

    fix logical_xcor() to cacl by FFT

--- kslib/misc.py
+++ kslib/misc.py
@@ -5,11 +5,27 @@ from scipy import fftpack as scifft
 
 # それぞれのスペクトルの横軸をlog2する関数
 def mylogx(xy:Tuple):
+    """(x,y) -> (l, y), l=log2(x)
+
+    Args:
+        xy (Tuple): (x,y)-data, x in N
+
+    Returns:
+        ly (Tuple): (l,y)-data, l in R
+    """
     xy = np.array(xy)
     lx = np.log2(xy[0].clip(1e-7))
     return lx,xy[1]
 
 def z(x):
+    """Z-standardize
+
+    Args:
+        x : array data
+
+    Returns:
+        z : standardize array data
+    """
     m = np.mean(x)
     s = np.std(x,ddof=1)
     xx = (x-m)/s
@@ -33,8 +49,9 @@ def myxcor(x:Tuple, y:Tuple, ti=1e-3, standardize=True, normalize=True):
     ly = np.floor((np.amax(y[0]) -t_begin)/ ti + 1).astype(int)
     N = lx if lx > ly else ly
     tl = lx + ly - 1
-    tau = (np.arange(tl)-ly)*ti
+    tau = (np.arange(tl)-ly+1)*ti
 
+    # 正規化
     if normalize == True:
         zx = z(x[1])
         zy = z(y[1])
@@ -42,22 +59,81 @@ def myxcor(x:Tuple, y:Tuple, ti=1e-3, standardize=True, normalize=True):
         zx = x[1]
         zy = y[1]
 
-    gx = np.zeros(lx)
+    # [i: 1,3,6]
+    # [x: 2,10,37]
+    # => [gx: 0,2,0,10,0,0,37] i.e. gx[t] == x[t] if t in i else 0
+    gx = np.zeros(lx, dtype='complex')
     gx[((x[0]-t_begin) / ti).astype(int)] = zx
-    gx = np.r_[np.zeros(ly-1), gx]
-
-    zy = z(y[1])
-    gy = np.zeros(ly)
+    gy = np.zeros(ly, dtype='complex')
     gy[((y[0]-t_begin) / ti).astype(int)] = zy
-    gy = np.r_[gy, np.zeros(lx-1)]
 
+    # 巡回畳み込みFFTによる相互相関
+    gx = np.r_[np.zeros(ly-1,dtype='complex'), gx]
+    gy = np.r_[gy, np.zeros(lx-1,dtype='complex')]
     Gx = scifft.fft(gx)
     Gy = scifft.fft(gy)
     Gxy = Gx * np.conj(Gy)
     cor = np.real(scifft.ifft(Gxy)) #/(N*ti)
+
+    # 標準化
     if standardize:
         tx, cx = myxcor(x,x,standardize=False)
         ty, cy = myxcor(y,y,standardize=False)
         cor /= np.sqrt(np.abs(cx[tx==0])) * np.sqrt(np.abs(cy[ty==0]))
 
     return tuple([tau, cor])
+
+def logical_xcor(x:Tuple, y:Tuple, ti=1e-3, standardize=True):
+    """cross correlation R_{x,y}(tau), in case
+        timings(indices) for x is  differ from those for y.
+
+    Args:
+        x (Tuple): t[i], x(t[i])
+        y (Tuple): t[i], y(t[i])
+        ti (_type_, optional): tick interval for t. Defaults to 1e-3.
+        standardize (bool, optional): return standardize. Defaults to True.
+
+    Returns:
+        correlation: tau, cor in cor(tau) := sum_i x(i-tau)*y(i)
+
+    >>> x = np.random.binomial(1,0.1,1000)
+    >>> i = 2*np.array(range(len(x)))
+    >>> y = x
+    >>> x = (i,x)
+    >>> y = (i+77,y)
+    >>> r = logical_xcor(x,y)
+    >>> print(r[0][np.argmax(r[1])])
+    -77.0
+    """
+    def booleanize(a):
+        if a.dtype != 'bool':
+            b = np.array([True if ai > a.mean()+a.std() else False for ai in a])
+        else:
+            b = np.copy(a)
+        return b
+
+    def bipolar(x):
+        q = len(set(x))
+        y = np.exp(1j * 2 * np.pi / q * x)
+        return y
+
+    zx = booleanize(x[1])
+    zx = bipolar(zx)
+    zx = (x[0],zx)
+
+    zy = booleanize(y[1])
+    zy = bipolar(zy)
+    zy = (y[0],zy)
+
+    return myxcor(zx,zy)
+
+if __name__ == '__main__':
+    x = np.random.binomial(1,0.1,1000)
+    i = 2*np.array(range(len(x)))
+    y = x
+    x = (i,x)
+    y = (i+77,y)
+    r = logical_xcor(x,y)
+    imax = np.argmax(r[1])
+    print(r[0][imax], r[1][imax])
+
