383034b93e4c80358451f6cfea9f0bd35209d1fd
Kotaro SONODA <kotaro1976@gmail.com>
2024-02-18 21:00:32 +0900

    fix doppler.py エミッタとレシーバの4ベクトル（時間，座標）を引数に取るように変更

--- kslib/doppler.py
+++ kslib/doppler.py
@@ -1,8 +1,8 @@
 import numpy as np
 from scipy import signal as scisig
 from kslib.mattoolbox import signal as matsig
-from kslib import reduct_frac
 from typing import List
+from kslib import interpolate as kinp
 
 def FireEngineSirenF0(t:List[float],num_harmonics:int=9):
     tt = t%6
@@ -25,128 +25,99 @@ def AmburanceSirenF0(t,num_harmonics=9):
     f0[off_region] = 780
     return np.array([(h+1)*f0 for h in range(num_harmonics)]).T
 
-
-def doppler_effect_from_signal(input, fs=44100, vs=40, vo=1/8, L=np.nan, D=20, precision=3, lenframe=64, loc_gain=False):
+def doppler_effect2(fs, duration, ve=1e2/9, vr=4/3, L=np.nan, D=20, vc = 340):
     """function applies changes in frequency that occurrs due to the doppler effect
-    Args:
-        input:
-        vs = 40 # in [km/h]
-        vo = 1/8 # in [km/m]
-        L = 100 # in [m]
-        D = 20 # in [m]
-        precision = 3 # doppler shift fcoeffの精度パラメータ
-                      #  (アップサンプリング 1/int(fcoeff):precision=0, (10^precision)/int(fcoeff*10^precision)
-        lenframe = 64 # frame length
-    Return:
-        output: # ドップラー効果のかかった信号（長さはinputと異なることもある）
-        fcoeff: # ドップラーシフト係数
-    """
 
-    nhop = lenframe
+        ```
+        |---------L----------|
+        (e)=======0==========
+        →         | :
+        ve        | D
+                  | :
+             vr ↑(r)-
+                  |
+        ```
 
-    N = len(input)
-    duration = N/fs
-
-    fcoef, gcoef, t = doppler_effect(fs,duration,vs,vo,L,D)
-
-    if loc_gain == True:
-        gain = gcoef
-    else:
-        gain = np.ones_like(t)
-
-    average_fcoeff = np.zeros_like(fcoef)
-    average_gain = np.zeros_like(gain)
-
-    for istart in range(0,N,nhop):
-        iend = np.amin([istart+lenframe,N])
-        len_i = iend-istart
-        avf = np.mean(fcoef[istart:iend])
-        average_fcoeff[istart:iend] = int(np.mean(fcoef[istart:iend])*(10**precision))/(10**precision)
-        avg = np.mean(gain[istart:iend])
-        average_gain[istart:iend] = avg
-
-        inp = input[istart:iend]
-        outp = avg * matsig.resample(inp, int(10**precision), int(avf*(10**precision)))
-
-        if istart == 0:
-            output = outp
-            ystart = istart
-        else:
-            output = np.concatenate([output, outp])
-        len_o = len(outp)
-        ystart += len_o
-
-    return output, fcoef, average_fcoeff, gain, average_gain
-
-def doppler_effect_from_F0(f0, gharmonics, fs=44100,vs=40, vo=1/8, L=np.nan, D=20, precision=3, lenframe=64, loc_gain=False):
-    """function applies changes in frequency that occurrs due to the doppler effect
-    Args:
-        f0 (duration(t-index), nchannel): 基本周波数
-        vs = 40 # in [km/h]
-        vo = 1/8 # in [km/m]
-        L = 100 # in [m]
-        D = 20 # in [m]
-        precision = 3 # doppler shift fcoeffの精度パラメータ
-                      #  (アップサンプリング 1/int(fcoeff):precision=0, (10^precision)/int(fcoeff*10^precision)
-        lenframe = 64 # frame length
-    Return:
-        output: # ドップラー効果のかかった信号（長さはinputと異なることもある）
-        fcoeff: # ドップラーシフト係数
-    """
-    N = len(f0)
-    duration = N/fs
-
-    fcoef, gcoef, t = doppler_effect(fs, duration, vs, vo, L, D)
-
-    nchannels = f0.shape[1]
-
-    if loc_gain == True:
-        gain = gcoef
-    else:
-        gain = np.ones_like(t)
-
-    output = np.full((N,),0.0)
-    for ichannel in range(nchannels):
-        phase_doppler = 2*np.pi*np.cumsum(fcoef*f0[:,ichannel])/fs # int f0(t)*fcoeff(t) dt
-        output +=  gharmonics[ichannel] * np.sin(phase_doppler)
-    output *= gain
-
-    return output, fcoef, gain
-
-def doppler_effect(fs, duration, vs=40, vo=1/8, L=np.nan, D=20, vc = 340):
-    """function applies changes in frequency that occurrs due to the doppler effect
     Args:
         fs : sampling frequency [Hz]
-        duration : coef duration [s]
-        vs = 40 # in [km/h]
-        vo = 1/8 # in [km/m]
-        L = 100 # in [m]
-        D = 20 # in [m]
+        duration : coef duration [s] 発音者の発音時間
+        ve = 1e2/9 # in [m/s] 発音車の速度 default 1e2/9[=40*1000/(60*60)]<=40km/h
+        vo = 4/3 # in [m/s] 受音者の速度 default 4/3[=80/60]<=80m/min
+        L = np.nan # in [m] 発音車の総走行距離 default NaN
+        D = 20 # in [m] 受音者の原点までの初期距離 default NaN
         vc = 340 # sound velocity [m/s]
     Return:
-        fcoef: # ドップラーシフト係数
-        gcoef: # 距離減衰係数
+        fcoefs: # ドップラーシフト係数 いわゆるchirp rates
+        gcoefs: # 距離減衰係数
         t: timestamp [s]
     """
     N = duration*fs
     t = np.arange(N)/fs
 
-    vs = vs*1000/(60*60)
-    vo = vo*1000/(60)
+    # Lを設定していないときは，信号長の真ん中で目の前を通過するようにLを決める
+    if np.isnan(L):
+        L = ve*duration/2
 
-    if np.isnan(L): # Lを設定していないときは，信号長の真ん中で目の前を通過するようにLを決める
-        L = vs*N/fs/2
+    # 発音車のx座標
+    xe = -L+ve*t
+    # 受音者のy座標
+    yr = -D+vr*t
 
-    xs = -L+vs*t
-    yo = -D+vo*t
+    ve_cos = -xe/np.sqrt(xe**2 + yr**2)
+    vr_cos = -yr/np.sqrt(xe**2 + yr**2)
 
-    vs_cos = -xs/np.sqrt(xs**2 + yo**2)
-    vo_cos = -yo/np.sqrt(xs**2 + yo**2)
+    ve_on = ve * ve_cos
+    vr_on = -vr * vr_cos
 
-    vs_on = vs * vs_cos
-    vo_on = -vo * vo_cos
-
-    fcoef = (vc-vo_on)/(vc-vs_on)
-    gcoef = 1/(xs**2 + yo**2)
+    fcoef = (vc-vr_on)/(vc-ve_on)
+    gcoef = 1/(xe**2 + yr**2)
 
     return fcoef, gcoef, t
+
+def doppler_effect(Xe,Xr):
+    """Chirp rates by Doppler effect
+
+    Args:
+        Xe (_type_): List of (te, xe, ye, ze)
+        Xr (_type_): List of (tr, xr, yr, zr)
+    """
+    te, *xe = Xe
+    xe = np.array(xe)
+    tr, *xr = Xr
+    xr = np.array(xr)
+
+    # Ve,vr
+    def diff(x,t):
+        vh = np.diff(x)/np.diff(t)
+        th = (t[:-1]+t[1:])/2
+        v = list()
+        for vhi in vh:
+            vi = kinp.lagrange((th,vhi),t)
+            v.append(vi)
+        v = np.array(v)
+        return v
+    ve = diff(xe,te)
+    vr = diff(xr,tr)
+
+    # Vc
+    dt = (tr-te).clip(1e-7)
+    vc = (xr-xe)/dt
+
+    def product(A,B):
+        return np.array([np.inner(a,b) for a,b in zip(A.T,B.T)])
+
+    ve2 = np.array([vei[0]**2+vei[1]**2+vei[2]**2 for vei in ve.T])
+    vr2 = np.array([vri[0]**2+vri[1]**2+vri[2]**2 for vri in vr.T])
+
+    ret1 = (1-product(vc,vr))/(1-product(vc,ve))
+    den = (1-vr2).clip(1e-7)
+    ret2 = (1-ve2)/den
+    ret2_sign = np.sign(ret2)
+    ret2_sign = np.array([1 if i>=0 else -1 for i in ret2_sign])
+    ret2_abs = np.abs(ret2)
+    ret2 = ret2_sign * np.sqrt(ret2_abs)
+
+    return ret1*ret2
+
+
+
